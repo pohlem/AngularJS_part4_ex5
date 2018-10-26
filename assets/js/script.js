1d68aaebab68cde6df13691574eaabd0b310c45f
pohlem <pohle.maxime@gmail.com>
2018-10-26 14:14:35 +0200

    Exercice 5 Finish

--- assets/js/script.js
+++
@@ -1,7 +0,0 @@
-var app = angular.module('showApp', []);
-
-app.controller('showController', function($scope, $http) {
-    $http.get("assets/json/voiture.json").then(function(response) {
-        $scope.voiture = response.data;
-    });
-});
